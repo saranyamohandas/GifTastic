03d35b8ec9981c75207203c6addb4c5875e3b292
saranyamohandas <saranyamohandas2@gmail.com>
2018-11-21 18:17:02 -0800

    Gif index/js/css/image/readme update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # GifTastic
-Gif API application
+----
+                          GifTastic App
+___
+
+### This is a giph application that has been developed using HTML,CSS,bootstrap,javascript and jquery library.
+
+#### Overview of the application -
+* On page load, App renders 10 buttons for cartoon & comics category. 
+* Upon clicking each button,application renders gif images from giphy https://developers.giphy.com thru AJAX call.
+* On successful AJAX call, images are dynamically generated.
+* Images are toggled between static and animated states.
+* Initially images appear in static state and on click, respective gif version of image is toggled and vice versa.
+* User can add additional cartoons or comics using add button.
+
+ 
+__Published git link to app__ : https://saranyamohandas.github.io/GifTastic/Gif API application
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
     
-   var topics = ['Minions',"Archer","Futurama"];
-    var apiData;
-    var searchQuery="minions";
+   var topics = ['Minions',"Archer","Futurama","Mermaid","tom and Jerry","inside out","popeye","the lion king","finding nemo","sesame street"];
+    
     renderBtn();
-    console.log("outside Ajax");
+   
     
   
-    $("#addBtn").click(function(){
+    $("#addBtn").on("click",function(){
         event.preventDefault();
         var getUserInp = $("#addInp").val().trim();
+        $("#addInp").val("");
         topics.push(getUserInp);
         console.log("getUserInp-",getUserInp);
+        console.log("topics -",topics)
         renderBtn();
     });
     
@@ -20,71 +21,87 @@ $(document).ready(function(){
         for (x in topics){ 
             var parentDiv = $(".main");
             var newBtn = $("<button>");
-            newBtn.text(topics[x]).addClass("btnList");
+            newBtn.text(topics[x]).addClass("btnList btn btn-primary");
             parentDiv.append(newBtn);
         }
         
     }
     
-    $(".btnList").click(function(){
-        //"http://api.giphy.com/v1/gifs/search?q=cartoons+comics+"+ searchQuery + "&limit=10&api_key=UZ94frJzDLfFAxztOhAOuif7KEHeG9u9",
+    $(".main").on("click",".btnList",function(){
         searchParam= $(this).text();
-        searchQuery =  "http://api.giphy.com/v1/gifs/search?q=cartoons+comics+"+ searchParam + "&limit=10&api_key=UZ94frJzDLfFAxztOhAOuif7KEHeG9u9";
-        console.log("new search",searchQuery)
-          $.ajax({
-      url: searchQuery,
-      method: "GET"
-    }).then(function(response) {
-      
-      console.log(response);
-        console.log("inside callback");
-        console.log(response.data[0]);
-        console.log(response.data[0].images.fixed_height.url);
+        searchQuery =  "http://api.giphy.com/v1/gifs/search?q=cartoons+comics+"+ searchParam + "&limit=10&rating=pg&api_key=UZ94frJzDLfFAxztOhAOuif7KEHeG9u9";
         
-        displayImg(response);
+          $.ajax({
+              url: searchQuery,
+              method: "GET"
+    }).then(function(response) {
+              console.log(response.data);
+              displayImg(response);
             
     });
         
     })
-     
+    // render appropriate images when respective button is clicked
     function displayImg(getData){
         
         var parentDiv = $(".image");
         parentDiv.empty();
-       // var imgDataAttr = ""
-     for (i=0;i<=10;i++){
+       
+     for (i=0;i<10;i++){
         //create div with class thumbnail
         var imgThumbnail = $("<div>");
         //create div with class caption & add rating as text from response
         var imgCaption = $('<div>');
-         //
+         //get rating to display in DOM
         var imgRating = getData.data[i].rating;
-           console.log(imgRating);
-        imgCaption.addClass('caption').text(imgRating );
+          
+        imgCaption.addClass('caption text-center').text("Rating - " +imgRating );
+         
          // add div with class thumbnail & append div with class caption as child
         imgThumbnail.addClass('thumbnail').append(imgCaption);
         // create img & add src from api
         var childImg = $("<img>");
-        var imgSrc = getData.data[i].images.fixed_height.url;
-        console.log("imgsrc-",imgSrc);
-        childImg.attr('src',imgSrc);
+        var imgSrcStill = getData.data[i].images.fixed_height_still.url;
+        var imgSrcAnimate = getData.data[i].images.fixed_height.url;
+         
+        //console.log("imgsrc-",imgSrc);
+        childImg.attr('src',imgSrcStill) ;
+        childImg.attr("data-still",imgSrcStill)
+        childImg.attr("data-animate", imgSrcAnimate)
+        childImg.attr("data-state", "still")
+        childImg.addClass("imgState");
         imgThumbnail.append(childImg);
-         //build div with class thumbnail with two child nodes created
+        //build div with class thumbnail with two child nodes created
         parentDiv.append(imgThumbnail);
-        
-         
-//        parentDiv.append(imgRating);
-//        newChild.attr("src",imgSrc);
-//        imgRating.append(newChild);
-//        console.log("image generate");
-//        console.log(parentDiv.append(newChild));
-         
+              
      }
+        
     }
+    // toggle image between still and animate url
+    $(".image").on("click",".thumbnail img", function(){
+    console.log(" Img clicked");
+      
+    var getImgState = $(this).attr("data-state");
+    
+             
+             if(getImgState == "still"){
+                 var getAnimateUrl = $(this).attr("data-animate")
+                 $(this).attr("data-state","animate");
+                 $(this).attr("src",getAnimateUrl);
+                 
+             } else {
+                 var getStillUrl = $(this).attr("data-still")
+                 $(this).attr("data-state","still");
+                 $(this).attr("src",getStillUrl);
+             }
+            
+         });
     
     
     
 });
+
+
 
 
 
